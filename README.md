5063508d32644f7339a7a9cdaa11e4543a9fe8b6
xiangqi01 <xiangqi01@cohl.com>
2025-04-26 23:47:07 +0800

    init

--- README.md
+++ README.md
@@ -1,94 +1,47 @@
-# Obsidian Sample Plugin
+# Open a Specific File
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+一个简单的 Obsidian 插件，让你可以快速打开指定的文件。
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+## 功能
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+- 📄 快速打开指定文件：通过命令或侧边栏图标快速打开你配置的文件
+- 📅 每日笔记：快速打开今天的每日笔记（需要启用 Daily Notes 插件）
 
-## First time developing plugins?
+## 安装
 
-Quick starting guide for new plugin devs:
+1. 打开 Obsidian 设置
+2. 进入社区插件
+3. 关闭安全模式（如果开启）
+4. 点击浏览并搜索 "Open a Specific File"
+5. 点击安装
+6. 在社区插件列表中启用该插件
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+## 使用方法
 
-## Releasing new releases
+### 配置快速打开文件
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+1. 打开设置
+2. 进入 "Open a Specific File" 设置页面
+3. 点击 "Add File Configuration" 添加文件配置
+4. 填写：
+   - 名称：显示名称
+   - 路径：文件在 vault 中的相对路径
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+### 打开文件的方式
 
-## Adding your plugin to the community plugin list
+- 使用命令面板（Ctrl/Cmd + P）搜索 "Open [文件名]"
+- 点击左侧栏的文件图标打开文件列表
 
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+## 系统要求
 
-## How to use
+- Obsidian v0.15.0 或更高版本
+- Daily Notes 插件（可选，用于每日笔记功能）
 
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
+## 支持
 
-## Manually installing the plugin
+如果你觉得这个插件有用，欢迎支持开发：
+- [GitHub Sponsors](https://github.com/tmkddskf)
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+## 许可证
 
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+本项目采用 MIT 许可证 - 详见 LICENSE 文件
--- main.ts
+++ main.ts
@@ -1,85 +1,75 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface FileConfig {
+	name: string;
+	path: string;
+	commandId: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface OpenSpecificFileSettings {
+	fileConfigs: FileConfig[];
+	enableDailyNote: boolean;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: OpenSpecificFileSettings = {
+	fileConfigs: [],
+	enableDailyNote: false
+}
+
+export default class OpenSpecificFile extends Plugin {
+	settings: OpenSpecificFileSettings;
+	private dailyNotesPlugin: any;
 
 	async onload() {
 		await this.loadSettings();
 
+		// Get the Daily Notes plugin
+		this.dailyNotesPlugin = (this.app as any).internalPlugins?.getPluginById('daily-notes');
+
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('file-text', 'Open Specific Files', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new FileListModal(this.app, this).open();
 		});
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		//ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		/*
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
+		*/
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		// Register commands for each configured file
+		this.registerFileCommands();
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		// Register daily note command if enabled
+		if (this.settings.enableDailyNote) {
+			this.registerDailyNoteCommand();
+		}
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new OpenSpecificFileSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
+		/*
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
+		*/
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		/*
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		*/
 	}
 
 	onunload() {
-
+		// Clean up commands when plugin is disabled
+		// No need to manually remove commands as they are automatically cleaned up
 	}
 
 	async loadSettings() {
@@ -89,16 +79,153 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	registerFileCommands() {
+		// No need to manually remove commands as they are automatically cleaned up
+		// when the plugin is disabled or reloaded
+
+		// Add command to open file list
+		this.addCommand({
+			id: 'open-specific-file:open-file-list',
+			name: 'Open specific file list',
+			callback: () => {
+				new FileListModal(this.app, this).open();
+			}
+		});
+
+		// Register commands for each configured file
+		this.settings.fileConfigs.forEach(config => {
+			this.addCommand({
+				id: config.commandId,
+				name: `Open ${config.name}`,
+				callback: () => {
+					this.openFile(config.path);
+				}
+			});
+		});
+	}
+
+	registerDailyNoteCommand() {
+		this.addCommand({
+			id: 'open-specific-file:open-daily-note',
+			name: 'Open Daily Note',
+			callback: async () => {
+				if (!this.dailyNotesPlugin) {
+					new Notice('Daily Notes plugin is not enabled');
+					return;
+				}
+
+				const options = this.dailyNotesPlugin.instance.options;
+				const today = new Date();
+				
+				// Format the date according to the Daily Notes plugin format
+				let formattedDate = options.format;
+				const dateTokens = {
+					'YYYY': today.getFullYear().toString(),
+					'YY': today.getFullYear().toString().slice(-2),
+					'MM': String(today.getMonth() + 1).padStart(2, '0'),
+					'M': String(today.getMonth() + 1),
+					'DD': String(today.getDate()).padStart(2, '0'),
+					'D': String(today.getDate()),
+					'ddd': ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][today.getDay()],
+					'dddd': ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][today.getDay()],
+					'HH': String(today.getHours()).padStart(2, '0'),
+					'H': String(today.getHours()),
+					'mm': String(today.getMinutes()).padStart(2, '0'),
+					'm': String(today.getMinutes()),
+					'ss': String(today.getSeconds()).padStart(2, '0'),
+					's': String(today.getSeconds())
+				};
+
+				// Replace all date tokens
+				Object.entries(dateTokens).forEach(([token, value]) => {
+					formattedDate = formattedDate.replace(new RegExp(token, 'g'), value);
+				});
+
+				// Construct the full path
+				let path = `${options.folder}${formattedDate}.md`;
+
+				// Check if the file exists
+				const file = this.app.vault.getAbstractFileByPath(path);
+				if (!(file instanceof TFile)) {
+					// If file doesn't exist, create it with template
+					if (options.template) {
+						const templateFile = this.app.vault.getAbstractFileByPath(options.template);
+						if (templateFile instanceof TFile) {
+							const templateContent = await this.app.vault.read(templateFile);
+							await this.app.vault.create(path, templateContent);
+						}
+					}
+				}
+				// if start with /, remove it
+				if (path.startsWith('/')) {
+					path = path.slice(1);
+				}
+				this.openFile(path);
+			}
+		});
+	}
+
+	async openFile(filePath: string) {
+		const file = this.app.vault.getAbstractFileByPath(filePath);
+		if (file instanceof TFile) {
+			// Check if the file is already open
+			const leaves = this.app.workspace.getLeavesOfType('markdown');
+			const existingLeaf = leaves.find(leaf => {
+				const view = leaf.view;
+				return view instanceof MarkdownView && view.file?.path === file.path;
+			});
+
+			if (existingLeaf) {
+				// If file is already open, focus on it
+				this.app.workspace.revealLeaf(existingLeaf);
+				const view = existingLeaf.view;
+				if (view instanceof MarkdownView) {
+					view.editor.focus();
+				}
+			} else {
+				// If file is not open, open it in a new leaf
+				await this.app.workspace.getLeaf(true).openFile(file);
+			}
+		} else {
+			new Notice(`File not found: ${filePath}`);
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class FileListModal extends Modal {
+	plugin: OpenSpecificFile;
+
+	constructor(app: App, plugin: OpenSpecificFile) {
 		super(app);
+		this.plugin = plugin;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.empty();
+
+		contentEl.createEl('h2', {text: 'Quick Open Files'});
+
+		const fileList = contentEl.createEl('div', {cls: 'file-list'});
+		
+		this.plugin.settings.fileConfigs.forEach(config => {
+			const fileItem = fileList.createEl('div', {cls: 'file-item'});
+			fileItem.createEl('button', {
+				text: config.name,
+				cls: 'file-button',
+			}).addEventListener('click', () => {
+				this.plugin.openFile(config.path);
+				this.close();
+			});
+		});
+
+		if (this.plugin.settings.fileConfigs.length === 0) {
+			fileList.createEl('div', {
+				text: 'No files configured. Please add file configurations in settings.',
+				cls: 'no-files-message'
+			});
+		}
 	}
 
 	onClose() {
@@ -107,28 +234,83 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class OpenSpecificFileSettingTab extends PluginSettingTab {
+	plugin: OpenSpecificFile;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: OpenSpecificFile) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
+		containerEl.createEl('h2', {text: 'File Configurations'});
+
+		// Add new file configuration
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Add New File')
+			.setDesc('Add a new file configuration')
+			.addButton(button => button
+				.setButtonText('Add')
+				.onClick(() => {
+					this.plugin.settings.fileConfigs.push({
+						name: 'New File',
+						path: '',
+						commandId: `open-specific-file:open-${Date.now()}`
+					});
+					this.plugin.saveSettings();
+					this.plugin.registerFileCommands();
+					this.display();
+				}));
+
+		// Display existing configurations
+		this.plugin.settings.fileConfigs.forEach((config, index) => {
+			const fileConfigContainer = containerEl.createEl('div', {cls: 'file-config-container'});
+			
+			new Setting(fileConfigContainer)
+				.setName(`File Configuration #${index + 1}`)
+				.addText(text => text
+					.setPlaceholder('Display Name')
+					.setValue(config.name)
+					.onChange(async (value) => {
+						config.name = value;
+						await this.plugin.saveSettings();
+						this.plugin.registerFileCommands();
+					}))
+				.addText(text => text
+					.setPlaceholder('File Path')
+					.setValue(config.path)
+					.onChange(async (value) => {
+						config.path = value;
+						await this.plugin.saveSettings();
+					}))
+				.addButton(button => button
+					.setButtonText('Delete')
+					.setWarning()
+					.onClick(async () => {
+						this.plugin.settings.fileConfigs.splice(index, 1);
+						await this.plugin.saveSettings();
+						this.plugin.registerFileCommands();
+						this.display();
+					}));
+		});
+
+		// Daily Note Configuration
+		containerEl.createEl('h2', {text: 'Daily Note Configuration'});
+		
+		new Setting(containerEl)
+			.setName('Enable Daily Note')
+			.setDesc('Enable the daily note feature (requires Daily Notes plugin to be enabled)')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enableDailyNote)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.enableDailyNote = value;
 					await this.plugin.saveSettings();
+					if (value) {
+						this.plugin.registerDailyNoteCommand();
+					}
 				}));
 	}
 }
